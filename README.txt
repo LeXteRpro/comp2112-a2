d194ba627046115eddf36cdce4ee9206d7d91435
LeXteRpro <lexter@hotmail.ca>
2016-10-16 22:20:03 -0400

    Home page, quiz page, three graphs, user input

---
+++ README.txt
@@ -0,0 +1,32 @@
+a) Why did you choose the medium you chose?
+
+I chose the medium of Canvas-based Data visualization because I have a practical use for it for a client. He has handed me his paper-based survey analysis program and requests that when I'm ready, I could turn it into a potential premium based product. This is a starting point, and not all the questions were entered for the sake of length in the assignment. I also find visualizing data to be one of the most important features in the business world, and I also have interest in using this skill for personal leisure.
+
+
+b) What did you know about the medium when you chose it?
+
+I knew pretty much nothing about this medium before I chose it. I mentioned my thought on creating graphs to a third year programming student before and he mentioned chart.js may be a great start, so I took this opportunity to try it. We used canvases briefly before in school so some of the concepts became familiar but overall I was brand new to this.
+
+
+c) What are all the resources (web pages, tutorials, videos, etc.) that you used to help you?
+
+1) Chart.js
+http://www.chartjs.org/docs/
+^ By far the primary source used
+
+2) Web Designer Depot 
+http://www.webdesignerdepot.com/2013/11/easily-create-stunning-animated-charts-with-chart-js/
+
+3) https://www.sitepoint.com/creating-beautiful-charts-chart-js/
+
+4) Google: Stack Overflow, MDN
+
+
+d) What was the most challenging part of the assignment, and how did you overcome it?
+The beginning was a challenge because setting up the node application was causing me loads of problems, wasn't able to get the application to be even be compatible with my computer. I worked past this by learning about the CDN links that I can just plug into my code like any other CSS or Javascript link.
+
+Learning how to customize the graph charts were at times challenging, and unfortunately my customization was limited because of time purposes. I would have liked to be more comfortable creating free custom code rather than editing a few words and sections here and there because I'm afraid to break everything.
+
+What I am happy about that I did on my own is that I wanted my graphs to read from user input on the page. I could not find any useful help with this online at all for some reason. I figured out how to create the variables and replace them in the code on my own.
+
+There are two pages, the Home Page, and the Quiz Page.View home page first.
--- scripts/app.js
+++ scripts/app.js
@@ -2,30 +2,33 @@
 // Document Ready
  $(document).ready(function(){
 
-			// Assign Variables
-        	var picture = 3;
-        	var plan = 0;
-        	var plow = 0;
-        	var plant = 0;
-        	var protect = 0;
-        	var prune = 0;
-        	var pollinate = 0;
-        	var propagate = 0;
+	// Assign Variables
+	var picture = 3;
+	var plan = 0;
+	var plow = 0;
+	var plant = 0;
+	var protect = 0;
+	var prune = 0;
+	var pollinate = 0;
+	var propagate = 0;
 
-// Create the Chart as the default state
-	myChartFunction();
+	// Hide Charts by Default
+
+	$("#myPolarAreaChartWrapper").hide();
+	$("#myRadarChartWrapper").hide();
+	$("#myBarChartWrapper").hide();
 
 	// Submit to create variables from user input
     $("#submit").click(function(){
 
-	var firstQuestionLength = $('#picture > div > input[type="checkbox"]:checked').length;
-	var secondQuestionLength = $('#plan > div > input[type="checkbox"]:checked').length;
-	var thirdQuestionLength = $('#plow > div > input[type="checkbox"]:checked').length;
-	var fourthQuestionLength = $('#plant > div > input[type="checkbox"]:checked').length;
-	var fifthQuestionLength = $('#protect > div > input[type="checkbox"]:checked').length;
-	var sixthQuestionLength = $('#prune > div > input[type="checkbox"]:checked').length;
-	var seventhQuestionLength = $('#pollinate > div > input[type="checkbox"]:checked').length;
-	var eighthQuestionLength = $('#propagate > div > input[type="checkbox"]:checked').length;
+	var firstQuestionLength = $('#picture div input[type="checkbox"]:checked').length;
+	var secondQuestionLength = $('#plan div input[type="checkbox"]:checked').length;
+	var thirdQuestionLength = $('#plow div input[type="checkbox"]:checked').length;
+	var fourthQuestionLength = $('#plant div input[type="checkbox"]:checked').length;
+	var fifthQuestionLength = $('#protect div input[type="checkbox"]:checked').length;
+	var sixthQuestionLength = $('#prune div input[type="checkbox"]:checked').length;
+	var seventhQuestionLength = $('#pollinate div input[type="checkbox"]:checked').length;
+	var eighthQuestionLength = $('#propagate div input[type="checkbox"]:checked').length;
 
     picture = firstQuestionLength;
     plan = secondQuestionLength;
@@ -37,27 +40,118 @@
     propagate = eighthQuestionLength;
 
 
-        // Call function to update chart
-        myChartFunction();
+	/* Hide Questions on page */
+	$("#questions-wrap").hide();
+
+	/* Show "Modify Chart Button" */
+	$("#modify-chart").show();
+
+
+    // Call function to update chart
+    myPolarAreaChartFunction();
+	/* Reveal the Chart and Wrapper HTML */
+	$("#myPolarAreaChartWrapper").show();
+
+    // Call function to update chart
+    myRadarChartFunction();
+	/* Reveal the Chart and Wrapper HTML */
+	$("#myRadarChartWrapper").show();
+
+    // Call function to update chart
+    myBarChartFunction();
+	/* Reveal the Chart and Wrapper HTML */
+	$("#myBarChartWrapper").show();
+
+
     });
 
 
+// When Submit is clicked, bring screen back to top
+if ($('#submit').length) {
+    var scrollTrigger = 50, // px
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > scrollTrigger) {
+                $('#submit').addClass('show');
+            } else {
+                $('#submit').removeClass('show');
+            }
+        };
+    backToTop();
+    $(window).on('scroll', function () {
+        backToTop();
+    });
+    $('#submit').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 100
+        }, 700);
+    });
+}
+
+
+	function myPolarAreaChartFunction() {
+		var cty = document.getElementById("myPolarAreaChart");
+		var myBarChart = new Chart(cty, {
+	    type: 'polarArea',
+	    data: {
+	        labels: ["Picture", "Plan", "Plow", "Plant", "Protect", "Prune", "Polinate", "Propagate"],
+	        datasets: [{
+	            label: 'Results',
+	            data: [picture, plan, plow, plant, protect, prune, pollinate, propagate],
+	            backgroundColor: [
+	                'rgba(255, 99, 132, 0.2)',
+	                'rgba(54, 162, 235, 0.2)',
+	                'rgba(255, 206, 86, 0.2)',
+	                'rgba(75, 192, 192, 0.2)',
+	                'rgba(153, 102, 255, 0.2)',
+	                'rgba(255, 159, 64, 0.2)'
+	            ],
+	            borderColor: [
+	                'rgba(255,99,132,1)',
+	                'rgba(54, 162, 235, 1)',
+	                'rgba(255, 206, 86, 1)',
+	                'rgba(75, 192, 192, 1)',
+	                'rgba(153, 102, 255, 1)',
+	                'rgba(255, 159, 64, 1)'
+	            ],
+	            borderWidth: 1
+	        }]
+	    },
+	    options: { 			        
+	    	title: {
+		            display: true,
+		            text: 'Polar Area'
+		        },
+
+		        
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero:true
+	                }
+	            }]
+	        }
+	    }
+	});
+}
+
 
 // Wrapping the Chart canvas in a function to be able to call it
-function myChartFunction() {
+function myRadarChartFunction() {
 
 // Assign the chart to a variable
-var ctx = document.getElementById("myChart");
+var ctx = document.getElementById("myRadarChart");
 
 		// Creating Chart
-		var myChart = new Chart(ctx, {
+		var myRadarChart = new Chart(ctx, {
 		    type: 'radar',
 		    data: {
 		        labels: ["Picture", "Plan", "Plow", "Plant", "Protect", "Prune", "Polinate", "Propagate"],
 		        datasets: [{
 		            label: 'Results',
 		            // Assigning the data points to our created variables
-		            data: [picture, plan, plow, plant, protect, prune, pollinate, propagate ],
+		            data: [picture, plan, plow, plant, protect, prune, pollinate, propagate],
 		            backgroundColor: [
 		                'rgba(255, 99, 132, 0.3)'
 		            ],
@@ -65,6 +159,8 @@ var ctx = document.getElementById("myChart");
 		                'rgba(255,99,132,1)'
 		            ],
 		            borderWidth: 1
+
+
 		        }
 
 		   // Secondary Graph Data
@@ -83,19 +179,65 @@ var ctx = document.getElementById("myChart");
 		    options: {
 			        title: {
 			            display: true,
-			            text: 'Data Graphs'
+			            text: 'Radar'
 		        },
 
 		        scales: {
 		            yAxes: [{
 		                ticks: {
-		                    display: false
+		                    display: true
 		                },
 		            }]
 		        }
 		    }
 		});
+
 	}
+
+
+	function myBarChartFunction() {
+		var ctb = document.getElementById("myBarChart");
+		var myBarChart = new Chart(ctb, {
+	    type: 'bar',
+	    data: {
+	        labels: ["Picture", "Plan", "Plow", "Plant", "Protect", "Prune", "Polinate", "Propagate"],
+	        datasets: [{
+	            label: 'Results',
+	            data: [picture, plan, plow, plant, protect, prune, pollinate, propagate],
+	            backgroundColor: [
+	                'rgba(255, 99, 132, 0.2)',
+	                'rgba(54, 162, 235, 0.2)',
+	                'rgba(255, 206, 86, 0.2)',
+	                'rgba(75, 192, 192, 0.2)',
+	                'rgba(153, 102, 255, 0.2)',
+	                'rgba(255, 159, 64, 0.2)'
+	            ],
+	            borderColor: [
+	                'rgba(255,99,132,1)',
+	                'rgba(54, 162, 235, 1)',
+	                'rgba(255, 206, 86, 1)',
+	                'rgba(75, 192, 192, 1)',
+	                'rgba(153, 102, 255, 1)',
+	                'rgba(255, 159, 64, 1)'
+	            ],
+	            borderWidth: 1
+	        }]
+	    },
+	    options: { 			        
+	    	title: {
+		            display: true,
+		            text: 'Bar'
+		        },
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero:true
+	                }
+	            }]
+	        }
+	    }
+	});
+}
 
 });
 
